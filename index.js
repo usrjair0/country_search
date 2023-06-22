dfd03f5970042438fe971e619d27d362b89b6922
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-06-22 20:50:29 -0300

    add some changes

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-let pais = document.getElementById('pais').value
-let card = document.getElementById('card')
-if(pais == ''){
-    card.style.display = "none"
+let pais = document.getElementById("pais").value;
+let card = document.getElementById("card");
+if (pais == "") {
+  card.style.display = "none";
 }
 function procurar() {
-    card.style.display = "block"
-    let pais = document.getElementById('pais').value
-    let finalURL = `https://restcountries.com/v3.1/name/${pais}?fullText=true`
-    console.log(finalURL)
-    fetch(finalURL)
-    .then(function(response){
-        return response.json()
+  card.style.display = "block";
+  let pais = document.getElementById("pais").value;
+  let finalURL = `https://restcountries.com/v3.1/name/${pais}?fullText=true`;
+  fetch(finalURL)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(data){
+    .then(function (data) {
+      let bandeira = document.getElementById("bandeira");
+      let nome = document.getElementById("nome");
+      let capital = document.getElementById("capital");
+      let cont = document.getElementById("cont");
+      let pop = document.getElementById("pop");
 
-        let bandeira = document.getElementById('bandeira')
-        let nome = document.getElementById('nome')
-        let capital = document.getElementById('capital')
-        let cont = document.getElementById('cont')
-        let pop = document.getElementById('pop')
-
-        nome.innerHTML = pais
-        capital.innerHTML = data[0].capital[0]
-        cont.innerHTML = data[0].continents[0]
-        pop.innerHTML = data[0].population
-        bandeira.src = data[0].flags.svg
-        return pais
-    })
+      nome.innerHTML = pais;
+      capital.innerHTML = data[0].capital[0];
+      cont.innerHTML = data[0].continents[0];
+      pop.innerHTML = data[0].population;
+      bandeira.src = data[0].flags.svg;
+    });
 }
